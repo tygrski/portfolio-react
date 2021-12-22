3e1a3453a715fa3f0b6f5a97385cd4d47937cac3
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-22 17:05:22 -0600

    nav component bug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,11 +4,11 @@ import { Button, Card, Row, Col } from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
 import './index.css'
+import Nav from './components/Nav/index'
 
 function App() {
   return (
-    <div>
-    </div>
+    <Nav></Nav>
   );
 }
 
---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Nav() {
+    <div>
+        <h1>Test</h1>
+        <p>GGGGGGGGGGGGGGGGGG</p>
+    </div>
+}
+
+export default Nav;
