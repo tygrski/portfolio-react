492c465c7fc08b6d35e47e2c52395289ebb1b087
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-22 15:59:41 -0600

    css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,13 @@
+import React from 'react';
+import 'materialize-css';
+import { Button, Card, Row, Col } from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
