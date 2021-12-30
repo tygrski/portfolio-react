dbe8613d9f67ff4ff8041fac8bd3a5b073774faa
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-30 17:54:55 -0600

    updated css resume/project coomponents

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -3,94 +3,88 @@ import React from "react";
 function Projects() {
   return (
     <div>
-      <h1>Portfolio</h1>
+      
 
       <section class="main-content">
-      <h2 class="section-me">Projects</h2>
-          <div class="my-info-container" id="my-work">
-            
+        <h2 class="section-project">Projects</h2>
+        <div class="my-info-container" id="my-work">
+          <a
+            className="portfolio-link"
+            href="https://github.com/ggamb/Beer-belly-developers-project-2/"
+            target="_blank"
+            alt="run-buddy waebpage image"
+          >
+            <div class="beer">
+              <h3>
+                <span class="workhr">Beer Belly</span>
+              </h3>
+            </div>
+          </a>
 
-            <a
-              className="portfolio-link"
-              href="https://github.com/ggamb/Beer-belly-developers-project-2/"
-              target="_blank"
-              alt="run-buddy waebpage image"
-            >
-              <div class="beer">
-                <h3>
-                  <span class="workhr">Beer Belly</span>
-                </h3>
-              </div>
-            </a>
+          <a
+            className="portfolio-link"
+            href="https://tygrski.github.io/soccer-finder/"
+            target="_blank"
+            alt="image of m pick-a-kick webpage"
+          >
+            <div class="soccer-finder">
+            <h3>
+                <span class="workhr">Pick-A-Kick</span>
+              </h3>
+            </div>
+          </a>
 
-            <a
-              className="portfolio-link"
-              href="https://tygrski.github.io/soccer-finder/"
-              target="_blank"
-              alt="image of m pick-a-kick webpage"
-            >
-              <div class="soccer-finder">
-                <h2>Pick-A-Kick</h2>
-              </div>
-            </a>
+          <a
+            className="portfolio-link"
+            href="https://github.com/tygrski/budget-tracker-PWA/"
+            target="_blank"
+            alt="budget-tracker image"
+          >
+            <div class="budget">
+              <h3>
+                <span class="workhr">Budget Tracker</span>
+              </h3>
+            </div>
+          </a>
 
-           
+          <a
+            className="portfolio-link"
+            href="https://github.com/tygrski/weatherDashboard/"
+            target="_blank"
+            alt="weather-dashboard"
+          >
+            <div class="weather">
+              <h3>
+                <span class="workhr">Weather Dashboard</span>
+              </h3>
+            </div>
+          </a>
 
-            <a
-              className="portfolio-link"
-              href="https://github.com/tygrski/budget-tracker-PWA/"
-              target="_blank"
-              alt="budget-tracker image"
-            >
-              <div class="budget">
-                <h3>
-                  <span class="workhr">Budget Tracker</span>
-                </h3>
-              </div>
-            </a>
-
-            <a
-              className="portfolio-link"
-              href="https://github.com/tygrski/weatherDashboard/"
-              target="_blank"
-              alt="weather-dashboard"
-            >
-              <div class="weather">
-                <h3>
-                  <span class="workhr">Weather Dashboard</span>
-                </h3>
-              </div>
-            </a>
-
-            <a
-              className="portfolio-link"
-              href="https://github.com/tygrski/Work-Task-List/"
-              target="_blank"
-              alt="Work-Task-List wabpage image"
-            >
-              <div class="work-task-list">
-                <h3>
-                  <span class="workhr">Work Task List</span>
-                </h3>
-              </div>
-            </a>
-
-            {/* this one is nopt displaying outside nav bar */}
-            <a
-              className="portfolio-link"
-              href="https://github.com/tygrski/budget-tracker-PWA/"
-              target="_blank"
-              alt="password-generator wabpage image"
-            >
-              <div class="password">
-                <h3>
-                  <span class="workhr">Password Generator</span>
-                </h3>
-              </div>
-            </a>
-           
-          </div>
-       
+          <a
+            className="portfolio-link"
+            href="https://github.com/tygrski/Work-Task-List/"
+            target="_blank"
+            alt="Work-Task-List wabpage image"
+          >
+            <div class="work-task-list">
+              <h3>
+                <span class="workhr">Work Task List</span>
+              </h3>
+            </div>
+          </a>
+          <a
+            className="portfolio-link"
+            href="https://github.com/tygrski/budget-tracker-PWA/"
+            target="_blank"
+            alt="password-generator wabpage image"
+          >
+            <div class="password">
+              <h3>
+                <span class="workhr">Password Generator</span>
+              </h3>
+            </div>
+          </a>
+        </div>
       </section>
     </div>
   );
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -3,29 +3,28 @@ import React from "react";
 function Resume() {
   return (
     <div className="resume-container">
-      <h1>Resume</h1>
+      
       <div className="resume-info">
-      <a href="https://drive.google.com/file/d/1j9ApKxir8TDPGIvZW5uz-woL_oLSsuNs/view?usp=sharing">
-        Click Here to Download PDF
-      </a>
-      <h2> My skills include :</h2>
-      <ul className="resume-list">
-        <li> HTML 5</li>
-        <li> CSS</li>
-        <li> Javascrpit</li>
-        <li> JQuery</li>
-        <li> Bootstrap</li>
-        <li> APIs</li>
-        <li> Node JS</li>
-        <li> JEST</li>
-        <li> Express JS</li>
-        <li> mySQL</li>
-        <li> NoSQL</li>
-        <li> Mongoose</li>
-        <li> REACT</li>
-        <li> Node JS</li>
-      </ul>
-    </div>
+        <a href="https://drive.google.com/file/d/1j9ApKxir8TDPGIvZW5uz-woL_oLSsuNs/view?usp=sharing"
+        className="resume-link"
+        >
+          Click Here to Download  my RESUME pdf
+        </a>
+        <h2 className="skills-title">My skills include :</h2>
+        <ul className="resume-list">
+          <li> HTML, CSS, Javascrpit</li>
+          <li> Bootstrap</li>
+          <li> JQuery</li>
+          <li> APIs</li>
+          <li> Node JS</li>
+          <li> Test Driven Development</li>
+          <li> Express JS</li>
+          <li> Sequelize</li>
+          <li> Mongoose</li>
+          <li>Progressive Web Apps</li>
+          <li> REACT</li>
+        </ul>
+      </div>
     </div>
   );
 }
